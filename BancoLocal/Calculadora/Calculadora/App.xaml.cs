b8e0480c48245fc6e1102d38754b0168929e9724
victor.hfcaetano <play.victor95@gmail.com>
2024-05-14 19:50:00 -0300

    sistema com banco

---
+++ BancoLocal/Calculadora/Calculadora/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculadora;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BancoLocal/Calculadora/Calculadora/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace Calculadora;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void BtSomar_Clicked(object sender, EventArgs e)
+    {
+        Double valor1 = Convert.ToDouble(etValor1.Text);
+        Double valor2 = Convert.ToDouble(etValor2.Text);
+        double total = valor1 + valor2;
+        DisplayAlert("Alert", "Total: " + valor1 + " + " + valor2 + " = " + total, "OK");
+
+    }
+
+    private void BtSubtrair_Clicked(object sender, EventArgs e)
+    {
+        Double valor1 = Convert.ToDouble(etValor1.Text);
+        Double valor2 = Convert.ToDouble(etValor2.Text);
+        double total = valor1 - valor2;
+        DisplayAlert("Alert", "Total: " + valor1 + " - " + valor2 + " = " + total, "OK");
+    }
+
+    private void BtDividir_Clicked(object sender, EventArgs e)
+    {
+        Double valor1 = Convert.ToDouble(etValor1.Text);
+        Double valor2 = Convert.ToDouble(etValor2.Text);
+        double total = valor1 / valor2;
+        DisplayAlert("Alert", "Total: " + valor1 + " / " + valor2 + " = " + total, "OK");
+    }
+
+    private void BtDividirResto_Clicked(object sender, EventArgs e)
+    {
+        int valor1 = Convert.ToInt32(etValor1.Text);
+        int valor2 = Convert.ToInt32(etValor2.Text);
+        double total = valor1 - valor2;
+        DisplayAlert("Alert", "Total: " + valor1 + " - " + valor2 + " = " + total, "OK");
+    }
+
+    private void BtMultiplicar_Clicked(object sender, EventArgs e)
+    {
+        Double valor1 = Convert.ToDouble(etValor1.Text);
+        Double valor2 = Convert.ToDouble(etValor2.Text);
+        double total = valor1 * valor2;
+        DisplayAlert("Alert", "Total: " + valor1 + " * " + valor2 + " = " + total, "OK");
+    }
+}
+
